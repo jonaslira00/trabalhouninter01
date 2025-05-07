01295281429aaf2f69af209174b15edc558764d4
Jonas Lira Santos <nona123lira@gmail.com>
2025-05-07 13:02:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() 
+{
+    const mensagem = "Você é capaz de conquistar coisas incríveis. Acredite em você!";
+    document.getElementById("mensagem").textContent = mensagem;
+}
